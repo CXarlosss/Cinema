5f81565c87451a31538a62a5bc03936e307e4eb1
Carlos <carlosdepet@gmail.com>
2025-03-27 15:52:19 +0100

    Dependencias-packjson

---
+++ cinema/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HEader hola que tal soy de APP:KS</h1>
+    </div>
+  );
+}
+
+export default App;
