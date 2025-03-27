9bf6fd206e397704f39c8686ebe3605799d5c356
Carlos <carlosdepet@gmail.com>
2025-03-27 16:15:29 +0100

    Routes

---
+++ cinema/src/assets/routes/AllRoutes.js
@@ -0,0 +1,21 @@
+ 
+
+ import { Routes, Route} from 'react-router-dom';
+ import { MovieList,MovieDetails,Search,PageNotFound } from '../pages';
+ import React from "react"  
+
+ export const AllRoutes = () => {
+    return( 
+        <div>
+            <Routes>
+                <Route path="" element={<MovieList apiPath="movie/now_playing" title="Home" />} />
+                <Route path='movie/:id' element={<MovieDetails />}/>
+                <Route path='movie/popular' element={<MovieList apiPath="movie/popular" title="Popular" />}/>
+                <Route path='movie/top' element={<MovieList apiPath="movie/top_rated" title="Top Rated" />}/>
+                <Route path="movies/upcoming" element={<MovieList apiPath="movie/upcoming" title="Upcoming" />} />
+                <Route path="search" element={<Search apiPath="search/movie"/>} />
+                <Route path="*" element={<PageNotFound />} />
+            </Routes>
+        </div>
+    )
+}
