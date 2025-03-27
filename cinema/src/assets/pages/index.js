e80fe8a7414130f6c779426d4ab4c9f02fbda136
Carlos <carlosdepet@gmail.com>
2025-03-27 16:08:41 +0100

    indexes

---
+++ cinema/src/assets/pages/index.js
@@ -0,0 +1,7 @@
+// @ts-nocheck
+
+export { MovieList } from "./MovieList";
+export {MovieDetails} from "./MovieDetails";
+export {Search} from "./Search";
+
+export {PageNotFound} from "./PageNotFound";
